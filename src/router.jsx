4adb5c56190bc4b58eb28f5a1900f4f3f457c677
eliasdiz <114044365+eliasdiz@users.noreply.github.com>
2025-02-28 14:28:04 -0500

    Elias (#2)
    * inicializacion del proyecto
    
    * login y crear usuario funcionales

--- src/router.jsx
+++ src/router.jsx
@@ -1,8 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import Login from "./components/Login";
+import Registro from "./components/Registro";
+import DashBoard from "./components/DashBoard";
 
 const router = createBrowserRouter([
-        { path:'/', element: <Login />}
+        { path:'/', element: <Login />},
+        { path:'/registro', element: <Registro />},
+        { path:'/dashboard', element: <DashBoard />},
 ])
 
 export default router
